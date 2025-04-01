99bb3567fa44e4a0b0f4948f7654a001172d6667
keyflux <elric.wu@hotmail.com>
2025-04-01 18:28:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -21,7 +21,7 @@ cd $(dirname "$0")
 
 echo "Parse flash type: $1"
 # simple check if partition table is valid
-if [ -z $( echo -n "$2" | grep '),-(data)') ]; then
+if [ -z $( echo -n "$2" | grep '(firmware)') ]; then
 	echo "Invalid mtd partition table!"
 	exit 1
 fi
